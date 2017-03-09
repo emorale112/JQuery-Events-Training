aa67ee7a4ba6d0114616cd8878d8605cc83908c7
erik morales <emorale112@gmail.com>
2017-03-09 00:50:05 -0600

    made it change

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $('#total').on('change', function () {
+    var num1 = parseInt($('#left').val());
+    var num2 = parseInt($('#right').val());
+
+    var total = num1 + num2;
+    $('#total').val(total).text(total);
+  });
+
 })
