abd7186334374adc18033be84ce2db3f29b93b25
erik morales <emorale112@gmail.com>
2017-03-09 01:10:36 -0600

    made it click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,8 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+$('#imperatives span').on('click', function() {
+  $('ul').append('<li>' + $(this).text() + ' at ' + Date.now());
+});
+
 })
